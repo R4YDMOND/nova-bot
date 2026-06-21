1e31142287536e7d239ee0cf7c733fe621acc3ec
Raydmond <R4YDMOND@yandex.ru>
2026-06-21 04:43:31 +0300

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,18 @@
-/** @type {import('tailwindcss').Config} */ 
-module.exports = { 
-  content: ["./src/**/*.{js,ts,jsx,tsx}"], 
-  theme: { extend: {} }, 
-  plugins: [] 
-} 
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        nova: {
+          400: '#67e8f9',
+          500: '#22d3ee',
+          600: '#06b6d4',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
