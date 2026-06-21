af5615cfeffe7b813ca06aff33865ef9095e21ec
Raydmond <R4YDMOND@yandex.ru>
2026-06-21 22:25:39 +0300

    Toaster.tsx

---
+++ frontend/src/components/Toaster.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { ToastProvider, ToastViewport } from '@/components/ui/Toast';
+
+export function Toaster() {
+  return (
+    <ToastProvider>
+      <ToastViewport />
+    </ToastProvider>
+  );
+}
