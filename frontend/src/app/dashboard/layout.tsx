16b53a5d984ca4b86c23be2a691f060412b71818
Raydmond <R4YDMOND@yandex.ru>
2026-06-22 03:11:23 +0300

    Update layout.tsx

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -7,11 +7,11 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-screen overflow-hidden bg-[rgb(var(--background))]">
+    <div className="flex h-screen overflow-hidden bg-[rgb(var(--background))] text-[rgb(var(--text))]">
       <Sidebar />
       <div className="flex-1 flex flex-col overflow-hidden">
         <Header />
-        <main className="flex-1 overflow-auto p-6 lg:p-8">
+        <main className="flex-1 overflow-auto p-6 lg:p-8 bg-[rgb(var(--background))]">
           {children}
         </main>
       </div>
