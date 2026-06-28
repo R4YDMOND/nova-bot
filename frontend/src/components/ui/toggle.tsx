05b344c17c89babb2cd2c6401ad13c8fed867ea9
R4YDMOND <R4YDMOND@yandex.ru>
2026-06-28 19:54:34 +0300

    fix: Switch поддержка onCheckedChange

--- frontend/src/components/ui/toggle.tsx
+++ frontend/src/components/ui/toggle.tsx
@@ -2,12 +2,24 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
-export interface SwitchProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface SwitchProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
+  onCheckedChange?: (checked: boolean) => void;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+}
 
 const Switch = React.forwardRef<HTMLInputElement, SwitchProps>(
-  ({ className, ...props }, ref) => (
+  ({ className, onCheckedChange, onChange, ...props }, ref) => (
     <label className="relative inline-flex items-center cursor-pointer">
-      <input type="checkbox" ref={ref} className="sr-only peer" {...props} />
+      <input
+        type="checkbox"
+        ref={ref}
+        className="sr-only peer"
+        onChange={(e) => {
+          onChange?.(e);
+          onCheckedChange?.(e.target.checked);
+        }}
+        {...props}
+      />
       <div className={cn(
         "w-11 h-6 bg-zinc-700 peer-focus:ring-2 peer-focus:ring-indigo-500 rounded-full peer",
         "peer-checked:bg-indigo-500 after:content-[''] after:absolute after:top-0.5 after:left-0.5",
